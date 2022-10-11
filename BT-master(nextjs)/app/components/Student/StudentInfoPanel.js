a94ecd59531228f3ee9c14f1b46922515622a325
Mike <47132583+3n0wd3n@users.noreply.github.com>
2022-10-11 16:55:49 +0200

    Update StudentInfoPanel.js

--- BT-master(nextjs)/app/components/Student/StudentInfoPanel.js
+++ BT-master(nextjs)/app/components/Student/StudentInfoPanel.js
@@ -1,6 +1,6 @@
 import React from 'react'
-
 import { StudentFontsHeaderBold, StudentFontsThin } from './StudentInfoPanel.style'
+import { Container } from '../CommonStyles'
 
 export default function InfoPanel({ data }) {
   console.log(data)
@@ -20,7 +20,11 @@ export default function InfoPanel({ data }) {
       <StudentFontsThin>role</StudentFontsThin>
       <StudentFontsHeaderBold>{data.role}</StudentFontsHeaderBold>
       <StudentFontsThin>your study plan</StudentFontsThin>
-      <StudentFontsHeaderBold>{data.plan}</StudentFontsHeaderBold>
+      <StudentFontsHeaderBold>{data.plan[0]}</StudentFontsHeaderBold>
+      <StudentFontsHeaderBold>{data.plan[1]}</StudentFontsHeaderBold>
+      <StudentFontsHeaderBold>{data.plan[2]}</StudentFontsHeaderBold>
+      <StudentFontsHeaderBold>{data.plan[3]}</StudentFontsHeaderBold>
+      <StudentFontsHeaderBold>{data.plan[4]}</StudentFontsHeaderBold>
       <StudentFontsHeaderBold>{test()}</StudentFontsHeaderBold>
 
     </>
